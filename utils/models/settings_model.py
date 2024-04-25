04c6231edaf4914f7aa3bd2e451488c4c55fc288
Seth-Schmidt <seth@powerloom.io>
2024-04-24 22:16:45 -0400

    add: initial data and settings models

---
+++ utils/models/settings_model.py
@@ -0,0 +1,55 @@
+from typing import List
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class RPCNodeConfig(BaseModel):
+    url: str
+
+
+class ConnectionLimits(BaseModel):
+    max_connections: int = 100
+    max_keepalive_connections: int = 50
+    keepalive_expiry: int = 300
+
+
+class RPCConfigBase(BaseModel):
+    full_nodes: List[RPCNodeConfig]
+    archive_nodes: Optional[List[RPCNodeConfig]]
+    force_archive_blocks: Optional[int]
+    retry: int
+    request_time_out: int
+    connection_limits: ConnectionLimits
+
+
+class RPCConfigFull(RPCConfigBase):
+    skip_epoch_threshold_blocks: int
+    polling_interval: int
+    semaphore_value: int = 20
+
+
+class Logs(BaseModel):
+    trace_enabled: bool
+    write_to_files: bool
+
+
+class SignerConfig(BaseModel):
+    address: str
+    private_key: str
+
+
+class TxSubmissionConfig(BaseModel):
+    enabled: bool = True
+    # relayer: RelayerService
+    signers: List[SignerConfig] = []
+
+
+class Settings(BaseModel):
+    source_private_key: str
+    source_address: str
+    source_chain_id: int
+    rpc: RPCConfigFull
+    snapshot_submissions: TxSubmissionConfig
+    logs: Logs
+    min_signer_value: float
